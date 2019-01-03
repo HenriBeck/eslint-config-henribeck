27699b296039335fdd9f483e3cfb85b306e22d2f
Henri <henribeck.dev@gmail.com>
2019-01-03 21:58:49 +0100

    - Turn on import/group-exports (#220)
    - Move disabling rules to the root configs

--- flow.js
+++ flow.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ['./lib/flowtype.js'] };
+module.exports = {
+  extends: ['./lib/flowtype.js'],
+
+  rules: {
+    // Turn off group export for flow projects as this can clash with exporting types
+    'import/group-exports': 'off',
+  },
+};
