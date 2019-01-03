c063465cc9ffe7cbb8156fc087c02c44c80d7e29
Henri Beck <henribeck.dev@gmail.com>
2019-01-03 20:50:53 +0100

    - Turn on import/group-exports - Move disabling rules to the root configs

--- typescript.js
+++ typescript.js
@@ -3,6 +3,17 @@ module.exports = {
 
   extends: ['./lib/typescript.js'],
 
+  rules: {
+    // This is currently buggy with the typescript parser
+    'no-useless-constructor': 'off',
+
+    // Will be handled by typescript directly
+    'no-undef': 'off',
+
+    // Also will be handled by typescript directly
+    'no-unused-vars': 'off',
+  },
+
   overrides: [{
     files: ['*.d.ts'],
     rules: {
